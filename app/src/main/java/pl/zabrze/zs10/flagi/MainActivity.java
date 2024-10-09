992f639c9bdab52aaed560de66c047b55f5886a6
AK01101001 <A.Krzak309803@gmail.com>
2024-10-09 15:52:21 +0200

    now

--- app/src/main/java/pl/zabrze/zs10/flagi/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/flagi/MainActivity.java
@@ -2,13 +2,35 @@ package pl.zabrze.zs10.flagi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int licznik;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewPolecenie);
+    }
+
+    public void zlaOdpwiedz(View view) 
+    {
+        Toast.makeText(this, R.string.zla, Toast.LENGTH_SHORT).show();
+    }
+
+    public void dobraOdpowiedz(View view)
+    {
+        Toast.makeText(this, R.string.dobra, Toast.LENGTH_SHORT).show();
+        licznik++;
+        view.setVisibility(View.INVISIBLE);
+        if (licznik==4)
+        {
+            textView.setText(R.string.podsumowanie);
+        }
     }
 }
